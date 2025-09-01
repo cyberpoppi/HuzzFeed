81137322ca7504f4cd3df964eaa5732c01300ce2
cyberpoppi <lh29149@email.vccs.edu>
2025-08-31 21:08:57 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,1 +1,18 @@
-console.log("Testing, Testing!");
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My First JavaScript</title>
+</head>
+<body>
+
+  <h1>Hello JavaScript!</h1>
+  <button onclick="sayHello()">Click Me!</button>
+
+  <script>
+    function sayHello() {
+      alert("You clicked the button! 🎉");
+    }
+  </script>
+
+</body>
+</html>
